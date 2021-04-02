46e60faf077c7caa837b67b5f722614b0ec96950
Ryan Plestid <ryanplestid@Ryans-MacBook-Pro.local>
2021-04-02 07:18:15 -0500

    adding all files

---
+++ Paper-I/Exclusion-Curves/left-plot-point.m
@@ -0,0 +1,2 @@
+(* Created with the Wolfram Language for Students - Personal Use Only : www.wolfram.com *)
+{0.01, 2.2239216081985154*^-9}
---
+++ Paper-I/Exclusion-Curves/left-point-plot.m
@@ -0,0 +1,2 @@
+(* Created with the Wolfram Language for Students - Personal Use Only : www.wolfram.com *)
+{0.01, 0.6035871311913813}
